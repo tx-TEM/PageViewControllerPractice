673155f096903b53b48ac92e70cb67289d439602
tx-TEM <highhigheggman@yahoo.co.jp>
2019-07-15 03:27:23 +0900

    gitignore

---
+++ PageViewControllerPractice/ScrollViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ScrollViewController.swift
+//  PageViewControllerPractice
+//
+//  Created by yoshiki-t on 2019/07/15.
+//  Copyright © 2019 yoshiki-t. All rights reserved.
+//
+
+import UIKit
+
+class ScrollViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
