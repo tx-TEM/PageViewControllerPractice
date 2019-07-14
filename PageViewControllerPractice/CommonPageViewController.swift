a286a7476374420e55c47eed753de05f7778ca16
tx-TEM <highhigheggman@yahoo.co.jp>
2019-07-15 03:28:24 +0900

    初期設定

--- PageViewControllerPractice/CommonPageViewController.swift
+++ PageViewControllerPractice/CommonPageViewController.swift
@@ -8,11 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CommonPageViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
