6271d3de4023daf53ffd85a7e92836b1fdb11a97
tx-TEM <highhigheggman@yahoo.co.jp>
2019-07-15 03:14:13 +0900

    Initial Commit

---
+++ PageViewControllerPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PageViewControllerPractice
+//
+//  Created by yoshiki-t on 2019/07/15.
+//  Copyright © 2019 yoshiki-t. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
